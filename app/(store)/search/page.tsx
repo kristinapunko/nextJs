4a9bca3b713e1848cf0197b0b9cd3731ea051952
kristinapunko <kristina.punko2006@gmail.com>
2025-07-11 22:30:41 +0300

    fix Search

--- app/(store)/search/page.tsx
+++ app/(store)/search/page.tsx
@@ -1,21 +1,38 @@
+// import ProductGrid from "@/components/ProductGrid";
+// import { searchProductByName } from "@/sanity/lib/products/searchProductByName";
+
+// async function SearchPage({
+//     searchParams
+// }:{
+//     searchParams:{
+//         query:string;
+//     }
+// }) {
+//     const {query} = searchParams
+//     const products = await searchProductByName(query)
+
+//     return(
+//         <div>
+//             <ProductGrid products={products}/>
+//         </div>
+//     )
+// }
+
+// export default SearchPage
+
+
 import ProductGrid from "@/components/ProductGrid";
 import { searchProductByName } from "@/sanity/lib/products/searchProductByName";
 
-async function SearchPage({
-    searchParams
-}:{
-    searchParams:{
-        query:string;
-    }
-}) {
-    const {query} = searchParams
-    const products = await searchProductByName(query)
+async function SearchPage({ searchParams }: { searchParams: Record<string, string | undefined> }) {
+    const query = searchParams.query || "";
+    const products = await searchProductByName(query);
 
-    return(
+    return (
         <div>
-            <ProductGrid products={products}/>
+            <ProductGrid products={products} />
         </div>
-    )
+    );
 }
 
-export default SearchPage
+export default SearchPage;
