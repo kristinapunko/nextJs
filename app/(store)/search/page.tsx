8b48b58791043c2708e6822e1c8e93908961f45a
kristinapunko <kristina.punko2006@gmail.com>
2025-07-11 22:42:20 +0300

    Fix searchParams type error in SearchPage

--- app/(store)/search/page.tsx
+++ app/(store)/search/page.tsx
@@ -20,19 +20,25 @@
 
 // export default SearchPage
 
-
 import ProductGrid from "@/components/ProductGrid";
 import { searchProductByName } from "@/sanity/lib/products/searchProductByName";
 
-async function SearchPage({ searchParams }: { searchParams: Record<string, string | undefined> }) {
-    const query = searchParams.query || "";
-    const products = await searchProductByName(query);
+// Define the expected type for searchParams
+interface SearchPageProps {
+  searchParams: Promise<Record<string, string | string[] | undefined>>;
+}
 
-    return (
-        <div>
-            <ProductGrid products={products} />
-        </div>
-    );
+async function SearchPage({ searchParams }: SearchPageProps) {
+  // Await the searchParams since it's a Promise
+  const resolvedSearchParams = await searchParams;
+  const query = resolvedSearchParams.query || "";
+  const products = await searchProductByName(query);
+
+  return (
+    <div>
+      <ProductGrid products={products} />
+    </div>
+  );
 }
 
 export default SearchPage;
